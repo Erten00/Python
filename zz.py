0fb8b55db0d170fed1c03f5220422d26819a6813
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-01-22 20:31:24 +0100

    edit

--- zz.py
+++ zz.py
@@ -13,7 +13,7 @@ for i in range(1,1000):
         count -= 1
 
     if (i % 10 ==0):
-        print(s + 'Happy New Year 2022)
+        print(s + 'Happy New Year 2022')
     else:
         print(s + '*')
 
