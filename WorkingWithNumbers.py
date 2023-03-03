bed7a58d4a9e939e7ae8dfe8ca264186c12c5e49
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-03 20:38:42 +0100

    .

--- WorkingWithNumbers.py
+++ WorkingWithNumbers.py
@@ -1,1 +1,12 @@
-print(2)
+my_num = 5
+print(my_num)  
+
+
+#from math import *
+
+#abs - Daje apsulutni broj
+#pow - Na (pow(4, 6))
+#max - Izbacuje veci (max(4, 6))
+#min - Izbacuje manji (min(4, 6))
+#round - Zaokruzuje (round(3.7))
+#sqrt - Koren (sqrt(36))
