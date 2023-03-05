8750726097b24b02f3d455bac89f7c2554cf26b7
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-05 22:15:10 +0100

    .

--- ListFuncions.py
+++ ListFuncions.py
@@ -2,4 +2,16 @@
 lucky_numbers = [4, 8, 15, 16, 23, 42]
 friends = ["Kevin", "Karen", "Jim","Oscar","Toby"]
 
-print(friends)
+#friends.extend(lucky_numbers)
+#friends.append("Creed")
+#friends.insert(1, "Creed")
+#friends.remove("Jim")
+#friends.clear()
+#friends.pop()
+#friends.sort()
+# lucky_numbers.sort()
+#lucky_numbers.reverse()
+#friends2 = friends.copy()
+
+#print(friends.index("Oscar"))
+#print(friends.count())
