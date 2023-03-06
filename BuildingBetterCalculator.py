6b6b96994af71b5ba91b5aeee2bae2519f173677
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-06 21:29:57 +0100

    .

---
+++ BuildingBetterCalculator.py
@@ -0,0 +1,16 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter secound number: "))
+op = input("Enter operator")
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("error ")
+
