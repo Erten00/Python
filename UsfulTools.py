365ea5f45153535a8ceb22207b0a6483c9088beb
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-07 19:04:48 +0100

    .

---
+++ UsfulTools.py
@@ -0,0 +1,11 @@
+import random
+
+feetInMile = 5280
+metersInKilometer = 1000
+beatles = ["John Lennon", "Paul McCartney", "George Herrison", "Ringo Star"]
+
+def GetFileExt(filename):
+    return filename[filename.intex(".") + 1:]
+
+def rool_dice(num):
+    return random.randint(1,num)
