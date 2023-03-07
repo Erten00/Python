3f36b03239bce3e793079b4700e707a45381c0e2
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-03-07 19:50:04 +0100

    .

--- Student.py
+++ Student.py
@@ -1,8 +1,11 @@
 class Student:
-
-    def __init__(self, name, major, gpa, isOnProbation):
+    def __init__(self, name, major, gpa):
         self.name = name
         self.major = major
         self.gpa = gpa
-        self.isOnProbation = isOnProbation
-        
+
+    def onHonorRoll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False        
