4416a924d662c1b3dd9aa1ce84928043d060f3b6
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-04-13 23:27:47 +0200

    .

---
+++ Intermediate Level/Dictionaries.py
@@ -0,0 +1,27 @@
+# Dictionary : Key-Value pairs, Unordered, Mutable
+
+mydict = {"name":"Max","age":28,"city":"New York"}
+print(mydict)
+
+#mydict2 = dict(name="Mary",age=27, city="Boston")
+#print(mydict2)
+
+#mydict["email"] = "something@xyz.com"
+
+#value = mydict["name"]
+#print(value)
+
+#del mydict["name"]
+#print(mydict)
+
+#if "name" in mydict:
+#    print(mydict["name"])
+
+#try:
+#    print(mydict["name"])
+#except:
+#    print("error")
+
+#for key, value in mydict.items():
+#    print(key, value)
+
