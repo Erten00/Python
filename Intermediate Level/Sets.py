0eb1459851c639d038bd967b970c069cabb6d2e5
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-04-14 00:40:26 +0200

    .

---
+++ Intermediate Level/Sets.py
@@ -0,0 +1,31 @@
+#Sets : unordered, mutable, no duplicates
+
+#myset = set("Hello")
+#print(myset)
+
+#myset = set()
+
+#myset.add(1)
+#myset.add(2)
+#myset.add(3)
+
+#myset.remove(3)
+#print(myset.pop())
+#myset.clear()
+
+#print(myset)
+
+###########################################
+
+odds = {1,3,5,7,9}
+evens = {0,2,4,6,8}
+primes = {2,3,5,7}
+
+u = odds.union(evens) #Unija
+print(u)
+
+i = odds.intersection(evens) #Presek
+print(i)
+
+diff = odds.difference(primes)
+print(diff)
