eba7be53ff9b6eda0338d67072fe37af84d71b0f
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-04-29 19:52:14 +0200

    .

--- Intermediate Level/Strings.py
+++ Intermediate Level/Strings.py
@@ -137,25 +137,3 @@ print(a)
 # f-Strings are evaluated at runtime, which allows expressions
 a = f"The value is {2*60}"
 print(a)
-
-#######################################################
-
-# since a string is immutable, adding strings with +,  or += always 
-# creates a new string, and therefore is expensive for multiple operations
-# --> join method is much faster
-from timeit import default_timer as timer
-my_list = ["a"] * 1000000
-
-# bad
-start = timer()
-a = ""
-for i in my_list:
-    a += i
-end = timer()
-print("concatenate string with + : %.5f" % (end - start))
-
-# good
-start = timer()
-a = "".join(my_list)
-end = timer()
-print("concatenate string with join(): %.5f" % (end - start))
