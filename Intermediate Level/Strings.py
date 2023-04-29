ef03e0506743eb7d1f4d061c39778c67f1caa86c
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-04-29 19:25:06 +0200

    .

--- Intermediate Level/Strings.py
+++ Intermediate Level/Strings.py
@@ -82,7 +82,7 @@ print("Hello".count("e"))
 message = "Hello World"
 new_message = message.replace("World", "Universe")
 print(new_message)
-
+ 
 # split the string into a list
 my_string = "how are you doing"
 a = my_string.split() # default argument is " "
