1f2e7376b402af26852c22818c7a932a776d8975
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-05-10 18:34:02 +0200

    .

--- Intermediate Level/Exceotuins.py
+++ Intermediate Level/Exceotuins.py
@@ -42,3 +42,80 @@ except ZeroDivisionError as e:
 except TypeError as e:
     print('A TypeError occured:', e)
 
+##########################################
+
+try:
+    a = 5 / 1
+except ZeroDivisionError as e:
+    print('A ZeroDivisionError occured:', e)
+else:
+    print('Everything is ok')
+
+##########################################
+
+try:
+    a = 5 / 1 # Note: No ZeroDivisionError here
+    b = a + '10'
+except ZeroDivisionError as e:
+    print('A ZeroDivisionError occured:', e)
+except TypeError as e:
+    print('A TypeError occured:', e)
+else:
+    print('Everything is ok')
+finally:
+    print('Cleaning up some stuff...')
+
+##########################################
+
+# ImportError
+import nonexistingmodule
+
+# NameError
+a = someundefinedvariable
+
+# FileNotFoundError
+with open('nonexistingfile.txt') as f:
+    read_data = f.read()
+
+# ValueError
+a = [0, 1, 2]
+a.remove(3)
+
+# TypeError
+a = 5 + "10"
+
+# IndexError
+a = [0, 1, 2]
+value = a[5]
+
+# KeyError
+my_dict = {"name": "Max", "city": "Boston"}
+age = my_dict["age"]
+
+##########################################
+
+# minimal example for own exception class
+class ValueTooHighError(Exception):
+    pass
+
+# or add some more information for handlers
+class ValueTooLowError(Exception):
+    def __init__(self, message, value):
+        self.message = message
+        self.value = value
+
+def test_value(a):
+    if a > 1000:
+        raise ValueTooHighError('Value is too high.')
+    if a < 5:
+        raise ValueTooLowError('Value is too low.', a) # Note that the constructor takes 2 arguments here
+    return a
+
+try:
+    test_value(1)
+except ValueTooHighError as e:
+    print(e)
+except ValueTooLowError as e:
+    print(e.message, 'The value is:', e.value)
+
+##########################################
