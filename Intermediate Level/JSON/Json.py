58bf49e538cd7653bfc98a548411fb1e90d565f3
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-05-15 21:01:33 +0200

    .

--- Intermediate Level/JSON/Json.py
+++ Intermediate Level/JSON/Json.py
@@ -41,6 +41,19 @@ z = 5 + 9j
 zJSON = json.dumps(z, default=encode_complex)
 print(zJSON)
 
+from json import JSONEncoder
+class ComplexEncoder(JSONEncoder):
+    
+    def default(self, o):
+        if isinstance(z, complex):
+            return {z.__class__.__name__: True, "real":z.real, "imag":z.imag}
+        # Let the base class default method handle other objects or raise a TypeError
+        return JSONEncoder.default(self, o)
+    
+z = 5 + 9j
+zJson = ComplexEncoder().encode(z)
+print(zJSON)
+
 ##########################################
 
 class User:
