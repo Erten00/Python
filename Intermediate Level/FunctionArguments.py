b2854386d919faa0c225c01907677d09d9c07cf1
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-06-04 22:23:55 +0200

    .

--- Intermediate Level/FunctionArguments.py
+++ Intermediate Level/FunctionArguments.py
@@ -1,11 +1,11 @@
-########################################
+#######################################
 
 def print_name(name): # name is the parameter
     print(name)
 
 print_name('Alex') # 'Alex' is the argument
 
-########################################
+#######################################
 
 def foo(a, b, c):
     print(a, b, c)
@@ -24,7 +24,7 @@ foo(1, b=2, c=3)
 # foo(1, b=2, 3) # positional argument after keyword argument
 # foo(1, b=2, a=3) # multiple values for argument 'a'
 
-########################################
+#######################################
 
 # default arguments
 def foo(a, b, c, d=4):
@@ -37,7 +37,7 @@ foo(1, b=2, c=3, d=100)
 # def foo(a, b=2, c, d=4):
 #     print(a, b, c, d)
 
-########################################
+#######################################
 
 def foo(a, b, *args, **kwargs):
     print(a, b)
@@ -54,7 +54,7 @@ print()
 # omitting of args or kwargs is also possible
 foo(1, 2, three=3)
 
-########################################
+#######################################
 
 def foo(a, b, *, c, d):
     print(a, b, c, d)
@@ -71,7 +71,7 @@ def foo(*args, last):
 
 foo(8, 9, 10, last=50)
 
-########################################
+#######################################
 
 def foo(a, b, c):
     print(a, b, c)
@@ -87,7 +87,7 @@ foo(**my_dict)
 
 # my_dict = {'a': 1, 'b': 2, 'd': 3} # not possible since wrong keyword
 
-########################################
+#######################################
 
 def foo1():
     x = number # global variable can only be accessed here
