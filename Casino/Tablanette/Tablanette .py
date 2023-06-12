48f3c8e7a0c8d20f98ce03b36e34fe0dcb6c99f5
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-06-12 23:18:17 +0200

    .

--- Casino/Tablanette/Tablanette .py
+++ Casino/Tablanette/Tablanette .py
@@ -32,42 +32,50 @@ while True:
     print("Player 2's turn:")
     print("Your hand:", player2_hand)
     card_index = int(input("Choose a card to play (enter the index): "))
-    card = player2_hand.pop(card_index)
-    board.append(card)
+    card_opponent = player2_hand.pop(card_index)
+    board.append(card_opponent)
 
     print("Board:", board)
+
+    # Check for a match
+    if card == card_opponent:
+        if len(deck) >= 2:
+            player1_hand.extend(deck[:2])
+            player2_hand.extend(deck[2:4])
+            deck = deck[4:]
+        else:
+            player1_hand.extend(deck)
+            player2_hand.extend(deck)
+            deck = []
 
     # Check if any player has no cards left
     if not player1_hand:
         player1_score += len(board)
         print("Player 1 wins the trick!")
         board = []
-        if len(deck) >= 4:
-            player1_hand = deck[:4]
-            deck = deck[4:]
-        else:
-            player1_hand = deck[:]
-            deck = []
     if not player2_hand:
         player2_score += len(board)
         print("Player 2 wins the trick!")
         board = []
+
+    # Check if all four cards have been played
+    if len(player1_hand) == 0 and len(player2_hand) == 0:
         if len(deck) >= 4:
-            player2_hand = deck[:4]
-            deck = deck[4:]
-        else:
-            player2_hand = deck[:]
+            player1_hand = deck[:4]
+            player2_hand = deck[4:8]
+            deck = deck[8:]
+        elif len(deck) > 0:
+            player1_hand = deck[:len(deck) // 2]
+            player2_hand = deck[len(deck) // 2:]
             deck = []
+        else:
+            break
 
     print()
     print("Scores:")
     print("Player 1:", player1_score)
     print("Player 2:", player2_score)
     print()
-
-    # Check if any player has played their last card and the deck is empty
-    if not player1_hand and not player2_hand and not deck:
-        break
 
 # Determine the winner of the game
 if player1_score > player2_score:
