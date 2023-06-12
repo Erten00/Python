4567b11a1ef6a21d57832bb271b2dd3a85c27bc6
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-06-12 23:47:51 +0200

    .

--- Casino/Tablanette/420.py
+++ Casino/Tablanette/420.py
@@ -1,3 +1,4 @@
+import random
     # Check if both players have empty hands
     if not player1_hand and len(deck1) >= 4:
         player1_hand = deck1[:4]
@@ -11,3 +12,16 @@
     elif not player2_hand and len(deck2) > 0:
         player2_hand = deck2[:]
         deck2 = []
+
+# Create two decks of cards
+ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+deck1 = [(rank) for rank in ranks]
+deck2 = [(rank) for rank in ranks]
+
+# Shuffle the decks
+random.shuffle(deck1)
+random.shuffle(deck2)
+
+# Deal the cards to the players
+player1_hand = deck1[:4]
+player2_hand = deck2[:4]
