f81777954d24674abe1bd01964b77f46b8ed6ebb
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-06-15 20:01:17 +0200

    .

--- Casino/Tablanette/420.py
+++ Casino/Tablanette/420.py
@@ -13,19 +13,6 @@ import random
         player2_hand = deck2[:]
         deck2 = []
 
-# Create two decks of cards
-ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-deck1 = [(rank) for rank in ranks]
-deck2 = [(rank) for rank in ranks]
-
-# Shuffle the decks
-random.shuffle(deck1)
-random.shuffle(deck2)
-
-# Deal the cards to the players
-player1_hand = deck1[:4]
-player2_hand = deck2[:4]
-
 # Check if player 1 played the last card on the board
     if card == board[-1] and card_opponent != board[-1]:
         player1_inventory.extend(board)
