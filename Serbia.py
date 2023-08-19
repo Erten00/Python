73ef7427ae606d406af3a77d4785141365b9b46e
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2023-08-19 22:51:28 +0200

    .

--- Serbia.py
+++ Serbia.py
@@ -21,7 +21,6 @@ def draw_serbian_flag():
     y = 100
     width = 400
     height = 200
-
     draw_rectangle(x, y, width, height/2, '#0C4076')
     draw_rectangle(x, y-height/2, width, height/2, '#D7141A')
 
