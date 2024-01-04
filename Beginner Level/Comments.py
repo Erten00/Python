369bb9c65cc26cff02d203c6880a2e57e52b285a
Erten Preljevic <90061278+Erten00@users.noreply.github.com>
2024-01-04 20:20:02 +0100

    .

--- Beginner Level/Comments.py
+++ Beginner Level/Comments.py
@@ -1,5 +1,13 @@
-'''
- This program is cool
+course = 'nehlebari'
+another = course[:]
+print(another)
+also = course[0:2]
+print(also)
+more = course[0:2]
+print(more)
+jos = course[0:2]
+print(jos)
 
-'''
-print("Comments are fun")
+print(f'{also} nemoj mi pobeci')
+print(f'{more} da jedu jeeeli')
+print(f'"{jos}"placa na prodaju, 0343330200')
